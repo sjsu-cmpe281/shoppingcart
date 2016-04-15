eece1c831697bd93869dda66a727d90943b33527
saratvemulapalli <sarat.vemulapalli@sjsu.edu>
2016-04-14 22:58:34 -0700

    Updated Riak Java client to store and fetch data with JSON

--- misc/Sarat/Code.space/RiakClient/src/riakclient/FetchObject.java
+++ misc/Sarat/Code.space/RiakClient/src/riakclient/FetchObject.java
@@ -9,37 +9,40 @@ package riakclient;
  *
  * @author saratvemulapalli
  */
-import java.net.UnknownHostException;
 import com.basho.riak.client.api.RiakClient;
 import com.basho.riak.client.api.commands.kv.FetchValue;
-import com.basho.riak.client.api.commands.kv.StoreValue;
 import com.basho.riak.client.core.query.Location;
 import com.basho.riak.client.core.query.Namespace;
+
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-public class RiakClient1 {
+
+public class FetchObject {
+
+    // { "foo":"Some String", "bar":"some other string","foobar":5 }
+    static class Pojo {
+        public String str;
+        public String str2;
+        public int value;
+    }
+
     public static void main(String [] args) throws UnknownHostException, ExecutionException, InterruptedException {
 
-        // Riak Client with supplied IP and Port
         List<String> sarat;
         sarat = new ArrayList();
         sarat.add("127.0.0.1");
         RiakClient client = RiakClient.newClient(8087, sarat);
-        
-        //Storing data 
         Location location = new Location(new Namespace("TestBucket"), "TestKey");
-        String myData = "This is my data";
-        StoreValue sv = new StoreValue.Builder(myData).withLocation(location).build();
-        StoreValue.Response svResponse = client.execute(sv);
-        
-        //Fetching data
+
         FetchValue fv = new FetchValue.Builder(location).build();
         FetchValue.Response response = client.execute(fv);
-        String value = response.getValue(String.class);
-        System.out.println(value);//*/
-        
+
+        // Fetch object as Pojo class (map json to object)
+        Pojo myObject = response.getValue(Pojo.class);
+        System.out.println(myObject.str);
+
         client.shutdown();
     }
-    
 }
