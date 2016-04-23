40742138ec238834c83ea419df200254a2478791
Rakesh Datta <rdatta@Rakeshs-MacBook-Pro.local>
2016-04-23 11:14:38 -0700

    HTTP Server Code 2nd iteration

--- misc/Rakesh/MYHTTPServer.js
+++ misc/Rakesh/MYHTTPServer.js
@@ -1,33 +1,89 @@
 /* CMPE 281: Team Project */
 
 var http = require("http");
+var port = "8090";
 
 var server = http.createServer(function(request, response) {
- if (request.method === "GET") {
-      if (request.url === "/mongo") {
-         console.log("[INFO] : mongo GET request");
-      } else if (request.url === "/riak") {
-         console.log("[INFO} : riak GET request");
+      var err = '0';
+
+      if (request.url === "/catalog") {
+          switch (request.method) {
+            case "GET"    :
+                            console.log("[INFO] : received mongo GET request");
+          		    break;
+	    case "POST"   :
+                            console.log("[INFO] : received mongo POST request");
+          		    break;
+
+	    case "PUT"    :
+                            console.log("[INFO] : received mongo PUT request");
+          		    break;
+
+	    case "DELETE" :
+                            console.log("[INFO] : received mongo DELETE request");
+          		    break;
+          
+            default       :
+                            console.log("[ERROR] : received unknown method type");
+                            err = '405';
+          		    break;
+	  }
+
+      } else if (request.url === "/shopcart") {
+          switch (request.method) {
+            case "GET"    :
+                            console.log("[INFO] : received riak GET request");
+          		    break;
+          
+	    case "POST"   :
+                            console.log("[INFO] : received riak POST request");
+          		    break;
+
+	    case "PUT"    :
+                            console.log("[INFO] : received riak PUT request");
+          		    break;
+
+	    case "DELETE" :
+                            console.log("[INFO] : received riak DELETE request");
+          		    break;
+
+            default       :
+                            console.log("[ERROR] : received unknown method type");
+			    err = '405';
+          		    break;
+	  }
       } else {
-         console.log("[ERROR]: Unknown GET request");
+         console.log("[ERROR]: Unknown Url");
+         err = '400';
       }
- } else if (request.method === "POST") {
-      if (request.url === "/mongo") {
-         console.log("[INFO] : mongo POST request");
-      } else if (request.url === "/riak") {
-         console.log("[INFO} : riak POST request");
-      } else {
-         console.log("[ERROR]: Unknown POST request");
-      }
+
+ switch (err) {
+   case "0"   : response.writeHead(200, {"Content-Type": "application/json"});
+                var respStr = "{'key' : 'value'}";
+                response.write(respStr);
+                response.end();
+                break;
+
+   case "400" : response.writeHead(400, {"Content-Type": "application/json"});
+                var respStr = "{'error' : 'url is incorrect'}";
+                response.write(respStr);
+                response.end();
+                break;
+
+   case "405" : response.writeHead(405, {"Content-Type": "application/json"});
+                var respStr = "{'error' : 'REST method type not allowed'}";
+                response.write(respStr);
+                response.end();
+                break;
+
+   default    : response.writeHead(503, {"Content-Type": "application/json"});
+                var respStr = "{'error' : 'Server is unavailable'}";
+                response.write(respStr);
+                response.end();
+                break;
  }
 
- response.writeHead(200, {"Content-Type": "application/json"});
-  
- var respStr = "{'key' : 'value'}";
+});
 
- response.write(respStr);
- response.end();
- });
-
-server.listen(8081);
-console.log("Server Started");
+server.listen(port);
+console.log("My Server Started in port "+port);
